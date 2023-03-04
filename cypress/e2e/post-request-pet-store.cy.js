2ccae28b4e8a5bcd870dc7f1c3010352c3f6db12
alex <aleksandar.stojanovic.91bgd@gmail.com>
2023-03-04 10:39:21 +0100

    added commands and env variables

---
+++ cypress/e2e/post-request-pet-store.cy.js
@@ -0,0 +1,113 @@
+import petBody from "../fixtures/petBody.json"
+import {petBodyTestData} from "../fixtures/petBodyTestData.json"
+const petBodyTestData2 = require("../fixtures/petBodyTestData2.json").postPetTests
+import api from "../fixtures/endpoints.json"
+const url = Cypress.env("url");
+
+describe('Pet store tests', () => {
+    xit('Post request - Create pet with valid name', () => {
+        cy.request({
+            method: "POST",
+            url: "https://petstore.swagger.io/v2/pet",
+            headers: {
+                accept: "application/json",
+                "content-type": "application/json"
+            },
+            body: {
+                "id": 123456,
+                "category": {
+                    "id": 0,
+                    "name": "string"
+                },
+                "name": "Ares",
+                "photoUrls": [
+                    "string"
+                ],
+                "tags": [
+                    {
+                        "id": 0,
+                        "name": "string"
+                    }
+                ],
+                "status": "available"
+            }
+        }).then(response => {
+            expect(response.status).eq(200)
+            expect(response.body.id).eq(123456)
+            expect(response.body.name).eq("Ares")
+            cy.log(response.body.tags.id)
+        })
+    })
+    xit('Post request - Create pet with empty name', () => {
+        cy.request({
+            method: "POST",
+            url: "https://petstore.swagger.io/v2/pet",
+            headers: {
+                accept: "application/json",
+                "content-type": "application/json"
+            },
+            body: {
+                "id": 123456,
+                "category": {
+                    "id": 0,
+                    "name": "string"
+                },
+                "name": "",
+                "photoUrls": [
+                    "string"
+                ],
+                "tags": [
+                    {
+                        "id": 0,
+                        "name": "string"
+                    }
+                ],
+                "status": "available"
+            }
+        }).then(response => {
+            expect(response.status).eq(400)
+            expect(response.body.errorMessage).eq("Name is required")
+        })
+    })
+
+    xit('Post request - Create pet with empty name', () => {
+        cy.postRequest(url+api.pet.postPet, petBody)
+            .then(response => {
+                expect(response.status).eq(200)
+                expect(response.body.id).eq(12345678)
+                expect(response.body.name).eq("Ares")
+            })
+    })
+    xit('Post request - Create pet with empty name', () => {
+        petBody.id = 123456789;
+        cy.postRequest(url+api.pet.postPet, petBody)
+            .then(response => {
+                expect(response.status).eq(200)
+                expect(response.body.id).eq(123456789)
+                expect(response.body.name).eq("Ares")
+            })
+    })
+
+    petBodyTestData.forEach((petBodyData) => {
+        xit("Post request - Create pet with different id's", () => {
+            cy.postRequest(url+api.pet.postPet, petBodyData)
+                .then(response => {
+                    expect(response.status).eq(200)
+                    expect(response.body.id).eq(petBodyData.id)
+                    expect(response.body.name).eq(petBodyData.name)
+                })
+        })
+    })
+
+    // Object.entries(petBodyTestData2).forEach(([key, value]) => {
+    //     it(key + " Post request - Create pet", () => {
+    //         cy.postRequest(url + api.pet.postPet, value)
+    //             .then(response => {
+    //                 expect(response.status).eq(200)
+    //                 expect(response.body.id).eq(value.id)
+    //                 expect(response.body.name).eq(value.name)
+    //             })
+    //     })
+    // })
+
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,16 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/**
+ * @method postRequest Sends a post request
+ * @param url endpoint url
+ * @param body json body
+ */
+Cypress.Commands.add('postRequest', (url,body) => {
+    return cy.request({
+        method: "POST",
+        url: url,
+        headers: {
+            accept: "application/json",
+            "content-type": "application/json"
+        },
+        body: body
+    })
+})
